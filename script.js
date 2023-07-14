dad76cbc33dbfdb9b7b7fc1d8b28d6303e5e0409
DylanDev <dylanm.ravel@gmail.com>
2023-07-13 18:25:18 -0600

    all code is complete

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+const apiKey = "d2359fb27eea3148a626bc7a884993a1";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function CheckWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+}
+
+searchBtn.addEventListener("click", () => {
+    console.log("test");
+    CheckWeather(searchBox.value);
+});
+*/
